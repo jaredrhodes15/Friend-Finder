0047729a089021a5b74bf610ce6e1c112174fb0b
Jared Rhodes <jaredrhodes15@gmail.com>
2019-03-27 16:27:34 -0600

    logic working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,84 @@
-// save application's data as an array of objects.
+var friends = [
+	{
+		name: 'Tom',
+		photo: 'https://www.thefashionisto.com/wp-content/uploads/2015/07/Tom-Cruise-Mission-Impossible-Rogue-Nation-London-Screening-Three-Piece-Suit-2015-Style-Picture-002.jpg',
+		scores: [
+			'2',
+			'5',
+			'6',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Lebron James',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Lebron_wizards_2017_%28cropped%29.jpg/1200px-Lebron_wizards_2017_%28cropped%29.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'DJ Khalid',
+		photo: 'http://thefader-res.cloudinary.com/private_images/w_760,c_limit,f_auto,q_auto:best/GettyImages-912840664_uoudnj/dj-khaled-ciroc-studios-la.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jen',
+		photo: 'http://2.bp.blogspot.com/-gnm05FWd0lc/TVQYU4p6Q7I/AAAAAAAAPSU/Gn5poU0khsM/s1600/Jennifer-Aniston-10.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Owen',
+		photo: 'http://static.cinemagia.ro/img/db/actor/00/02/16/owen-wilson-938266l.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
 
+module.exports = friends;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,27 @@
-// Include GET route for /survey which should display the survey page
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
 
-// a catch all route that leads to home.html
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+
+};
